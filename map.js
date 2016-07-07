500c009404d052dff05293c023be0706ae542f1b
Julian Binder <julian.binder@mail.mcgill.ca>
2016-07-07 00:39:18 +0000

    added bit more to maps

--- map.js
+++ map.js
@@ -38,68 +38,84 @@ spawnWarrior (challenge): using the Warrior constructor that you created in the 
 spawnWarrior that takes a warrior object and a x and y position. This method of the Map should take the warrior object and 
 associate it to the appropriate Tile.
 */
-function Tile (type) {
-   this.type = type;
+function Tile(type) {
+    this.type = type;
 }
 
 Tile.prototype = {
-   isWalkable: function (type) {
-       if (this.type === "grass" || this.type === "sand") {
-           return true;
-       } 
-       else {
-           return false;
-       }
-   }
+    isWalkable: function(type) {
+        if (this.type === "grass" || this.type === "sand") {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
 
 
-function randomType () {
-   var type;
-   var randomTypes = Math.random();
-   if (randomTypes < 0.33) {
-       return "grass";
-   }
-   if (randomTypes > 0.33 && randomTypes < 0.66) {
-       return "sand";
-   }
-   if (randomTypes > 0.66) {
-       return "water";
-   }
+function randomType() {
+    var type;
+    var randomTypes = Math.random();
+    if (randomTypes < 0.33) {
+        return "grass";
+    }
+    if (randomTypes > 0.33 && randomTypes < 0.66) {
+        return "sand";
+    }
+    if (randomTypes > 0.66) {
+        return "water";
+    }
 };
 
-function Map (width, height) {
-   if (!isNaN(width)) {
-       this.width = width;
-   }
-   if (!isNaN(height)) {
-      this.height = height;
-   }
-   
-   }
-   
-   
-   Map.prototype = {
-       generateMap: function () {
-       var emptyArray = [];
-   
-   for (var x = 0; x < this.width; x++) {
-       var innerArr = [];
-       for (var y = 0; y < this.height; y++) {
-           var u = new Tile(randomType());
-               innerArr.push(u.type);
-           }
+function Map(width, height) {
+    if (!isNaN(width)) {
+        this.width = width;
+    }
+    if (!isNaN(height)) {
+        this.height = height;
+    }
+
+}
+
+
+Map.prototype = {
+    generateMap: function() {
+        var emptyArray = [];
+
+        for (var x = 0; x < this.width; x++) {
+            var innerArr = [];
+            for (var y = 0; y < this.height; y++) {
+                var u = new Tile(randomType());
+                innerArr.push(u.type);
+            }
             emptyArray.push(innerArr);
-       }
-       this.tiles = emptyArray;
-   }
-   }
-   
-var map = new Map(4,4);
+        }
+        this.tiles = emptyArray;
+    }
+};
+
+    Map.prototype.getWalkableOutput: function(emptyArray) {
+        for (var i = 0; i <= emptyArray.length; i++) {
+            if (emptyArray.length[i] === "grass") {
+                return "O"
+            }
+            if (emptyArray.length[i] === "sand") {
+                return "O"
+            }
+            if (emptyArray.length[i] === "water") {
+                return "X"
+            }
+        }
+    }
+}
+
+var map = new Map(4, 4);
 map.generateMap();
 console.log(map);
 
-     /*  getWalkableOutput: this method should iterate over the this.tiles 2D array, and output in the console a rectangle 
+
+/*  getWalkableOutput: this method should iterate over the this.tiles 2D array, and output in the console a rectangle 
 representing the "walkability" of the map. For walkable tiles, use an O, and for unwalkable tiles use an X. For example, 
 your output could look something like this for a 4x4 map:
 OOOO
